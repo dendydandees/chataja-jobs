929c998f44a9030d0caaf2b3fddd81e4118d1fa3
dendydandees <dendydharmawanq@gmail.com>
2020-12-29 16:46:55 +0700

    add dashboard employer page

---
+++ components/nav/MenuEmployer.vue
@@ -0,0 +1,209 @@
+<template>
+  <nav>
+    <!-- navigation bar -->
+    <v-app-bar app hide-on-scroll height="80" color="#2c4d63">
+      <v-container>
+        <div class="d-flex justify-between align-center">
+          <!-- logo chat aja jobs -->
+          <nuxt-link to="/">
+            <v-toolbar-title>
+              <img src="/cajobs-logo.png" alt="ChatAja Jobs" height="100%" />
+            </v-toolbar-title>
+          </nuxt-link>
+          <!-- end logo chat aja jobs -->
+
+          <v-spacer v-if="$vuetify.breakpoint.smAndDown"></v-spacer>
+
+          <v-app-bar-nav-icon
+            v-if="$vuetify.breakpoint.smAndDown"
+            class="float-right"
+            @click.stop="drawer = !drawer"
+          >
+            <img
+              src="/navigation-icon-white.svg"
+              alt="Navigation Icon"
+              height="100%"
+            />
+          </v-app-bar-nav-icon>
+
+          <!-- nav menu -->
+          <div v-if="$vuetify.breakpoint.mdAndUp" style="width: 100%">
+            <nav>
+              <v-row dense align="center" justify="space-between" class="ml-2">
+                <v-col cols="3">
+                  <!-- search field -->
+                  <v-text-field
+                    label="Pencarian"
+                    solo
+                    hide-details
+                  ></v-text-field>
+                  <!-- end search field -->
+                </v-col>
+                <v-col cols="5" class="text-center">
+                  <v-btn
+                    to="/employer/dashboard"
+                    text
+                    color="white"
+                    large
+                    exact
+                  >
+                    Job Vacancy
+                  </v-btn>
+                  <v-btn
+                    to="/employer/applicants"
+                    text
+                    color="white"
+                    large
+                    exact
+                  >
+                    Applicants
+                  </v-btn>
+                </v-col>
+
+                <!-- username, notifications, settings -->
+                <v-col cols="4">
+                  <v-row no-gutters align="center" justify="space-between">
+                    <v-col cols="6">
+                      <span class="subtitle-1 white--text">
+                        Selamat Datang
+                        <span
+                          class="font-weight-bold text-truncate d-inline-block"
+                          style="width: 100%"
+                        >
+                          Perusahaan
+                        </span>
+                      </span>
+                    </v-col>
+                    <v-col cols="auto">
+                      <v-btn icon color="white">
+                        <v-icon>mdi-bell</v-icon>
+                      </v-btn>
+                    </v-col>
+                    <v-col cols="auto">
+                      <v-btn icon color="white">
+                        <v-icon>mdi-cog</v-icon>
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-col>
+                <!-- end username, notifications, settings -->
+              </v-row>
+            </nav>
+          </div>
+          <!-- end nav menu -->
+        </div>
+      </v-container>
+    </v-app-bar>
+    <!-- end navigation bar -->
+
+    <!-- side bar -->
+    <v-navigation-drawer
+      v-if="$vuetify.breakpoint.smAndDown"
+      v-model="drawer"
+      app
+      right
+      clipped
+      width="100%"
+      temporary
+      color="#2c4d63"
+    >
+      <v-list nav>
+        <v-list-item-group v-model="group">
+          <nav>
+            <v-row no-gutters align="center">
+              <v-col cols="6">
+                <!-- logo chat aja jobs -->
+                <nuxt-link to="/">
+                  <v-toolbar-title>
+                    <img
+                      src="/cajobs-logo.png"
+                      alt="ChatAja Jobs"
+                      height="100%"
+                    />
+                  </v-toolbar-title>
+                </nuxt-link>
+                <!-- end logo chat aja jobs -->
+              </v-col>
+              <v-col cols="6" class="text-right">
+                <v-btn icon color="white" large @click="drawer = !drawer">
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+
+            <!-- username, notifications, settings -->
+            <v-row class="my-4">
+              <v-col>
+                <span class="subtitle-1 white--text">
+                  Selamat Datang
+                  <span class="font-weight-bold">Perusahaan</span>
+                </span>
+              </v-col>
+              <v-col cols="auto">
+                <v-btn icon color="white">
+                  <v-icon>mdi-bell</v-icon>
+                </v-btn>
+              </v-col>
+              <v-col cols="auto">
+                <v-btn icon color="white">
+                  <v-icon>mdi-cog</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+            <!-- end username, notifications, settings -->
+
+            <!-- search field -->
+            <v-text-field
+              label="Pencarian"
+              solo
+              hide-details
+              class="my-4"
+            ></v-text-field>
+            <!-- end search field -->
+
+            <v-btn
+              to="/employer/dashboard"
+              text
+              color="white"
+              block
+              large
+              exact
+              class="my-2"
+            >
+              Job Vacancy
+            </v-btn>
+            <v-btn
+              to="/employer/applicants"
+              text
+              color="white"
+              block
+              large
+              exact
+              class="my-2"
+            >
+              Applicants
+            </v-btn>
+          </nav>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- end side bar -->
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+    isProcessing: false,
+  }),
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
+}
+</script>
+
+<style></style>
--- components/nav/Navbar.vue
+++ components/nav/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <MenuProfile v-if="hasAccount || $route.name.includes('profile')" />
+    <MenuEmployer v-else-if="hasAccount || $route.name.includes('dashboard')" />
     <MenuSign v-else />
   </nav>
 </template>
@@ -8,11 +9,13 @@
 <script>
 import MenuSign from '@/components/nav/MenuSign'
 import MenuProfile from '@/components/nav/MenuProfile'
+import MenuEmployer from '@/components/nav/MenuEmployer'
 
 export default {
   components: {
     MenuSign,
     MenuProfile,
+    MenuEmployer,
   },
   data: () => ({
     hasAccount: false,
