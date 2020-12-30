98e4700dd750119d097994f85e1422de0dfb55a0
dendydandees <dendydharmawanq@gmail.com>
2020-12-31 00:29:10 +0700

    add notifications page for employer

---
+++ pages/employer/notifications.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container tag="section">
+    <v-row align="center">
+      <v-col cols="12" class="mt-7 mb-2">
+        <h1 class="display-1">Notifikasi</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
